4fb52dc31cf58447b6375cd6cff93b6a4c2b8360
John Lees <lees.john6@gmail.com>
2022-12-02 17:07:48 +0000

    Finish CLI design

--- src/cli.rs
+++ src/cli.rs
@@ -1,4 +1,6 @@
-use clap::{Parser, Subcommand, ArgGroup};
+use clap::{Parser, Subcommand, ArgGroup, ValueEnum};
+
+extern crate num_cpus;
 
 fn valid_kmer(s: &str) -> Result<usize, String> {
     let k: usize = s
@@ -9,6 +11,39 @@ fn valid_kmer(s: &str) -> Result<usize, String> {
     } else {
         Ok(k)
     }
+}
+
+fn zero_to_one(s: &str) -> Result<f64, String> {
+    let f: f64 = s
+        .parse()
+        .map_err(|_| format!("`{}` isn't a valid frequency", s))?;
+    if f < 0.0 || f > 1.0 {
+        Err(format!("Frequency must be between 0 and 1 (inclusive)"))
+    } else {
+        Ok(f)
+    }
+}
+
+fn valid_cpus(s: &str) -> Result<usize, String> {
+    let threads: usize = s
+        .parse()
+        .map_err(|_| format!("`{}` isn't a valid number of cores", s))?;
+    let max_threads = num_cpus::get();
+    if threads < 1 || threads > max_threads {
+        Err(format!("Threads must be between 1 and {}", max_threads))
+    } else {
+        Ok(threads)
+    }
+}
+
+#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum)]
+enum FileType {
+    /// Variant call format
+    Vcf,
+    /// Binary call format
+    Bcf,
+    /// FASTA alignment
+    Aln,
 }
 
 #[derive(Parser)]
@@ -44,43 +79,104 @@ enum Commands {
         #[arg(short, value_parser = valid_kmer, default_value_t = 17)]
         k: usize,
 
-        /// Ignore reverse complement (all contigs are oriented along same strand).
+        /// Ignore reverse complement (all contigs are oriented along same strand)
         #[arg(long, default_value_t = false)]
         single_strand: bool,
 
         /// Number of CPU threads
-        #[arg(long, default_value_t = 1)]
+        #[arg(long, value_parser = valid_cpus, default_value_t = 1)]
         threads: usize,
      },
-    // ska merge <files.skf>... -o <prefix>
     /// Combine multiple split k-mer files
-    Merge { name: Option<String> },
-    // ska delete <file.skf> -l <name_list>
+    Merge {
+        /// List of input split-kmer (.skf) files
+        seq_files: Vec<String>,
+
+        /// Output prefix
+        #[arg(short)]
+        output: String,
+    },
+    #[command(group(
+        ArgGroup::new("input")
+            .required(true)
+            .args(["names", "file_list"]),
+    ))]
     /// Remove samples from a split k-mer file
-    Delete { name: Option<String> },
-    // ska weed <file.skf> --seqs <list.fa>
+    Delete {
+        /// Split-kmer (.skf) file to operate on
+        skf_file: String,
+
+        /// File listing sample names to remove
+        #[arg(short, group = "input")]
+        file_list: Option<String>,
+
+        /// List of sample names to remove
+        #[arg(group = "input")]
+        names: Option<Vec<String>>,
+    },
     /// Remove k-mers from a split k-mer file
-    Weed { name: Option<String> },
-    // ska align <input_files>... <-o output.aln> [--min <freq>] [--const-sites] [--cpus <cpus>]
+    Weed {
+        /// Split-kmer (.skf) file to operate on
+        skf_file: String,
+
+        /// A FASTA file containing sequences to remove
+        weed_file: String
+    },
     /// Write an unordered alignment
-    Align { name: Option<String> },
-    // ska map <ref.fa> <input_files>... <-o output.aln> [--min <freq>] [--const-sites] [--cpus <cpus>]
+    Align {
+        /// A .skf file, or list of .fasta files
+        input: Option<Vec<String>>,
+
+        /// Output prefix
+        #[arg(short)]
+        output: String,
+
+        /// Minimum fraction of samples a k-mer has to appear in
+        #[arg(short, value_parser = zero_to_one, default_value_t = 0.9)]
+        min_freq: f64,
+
+        /// Output constant middle base sites
+        #[arg(long, default_value_t = false)]
+        const_sites: bool,
+
+        /// Number of CPU threads
+        #[arg(long, value_parser = valid_cpus, default_value_t = 1)]
+        threads: usize,
+    },
     /// Write an ordered alignment using a reference sequence
-    Map { name: Option<String> },
-    // ska nk file.skf <--full-info>
+    Map {
+        /// Reference FASTA file to map to
+        reference: String,
+
+        /// A .skf file, or list of .fasta files
+        input: Option<Vec<String>>,
+
+        /// Output prefix
+        #[arg(short)]
+        output: String,
+
+        #[arg(long, value_enum, default_value_t = FileType::Aln)]
+        output_format: FileType,
+
+        /// Output constant middle base sites
+        #[arg(long, default_value_t = false)]
+        const_sites: bool,
+
+        /// Number of CPU threads
+        #[arg(long, value_parser = valid_cpus, default_value_t = 1)]
+        threads: usize,
+    },
     /// Get the number of k-mers in a split k-mer file, and other information
-    Nk { name: Option<String> },
+    Nk {
+        /// Split-kmer (.skf) file to operate on
+        skf_file: String,
+
+        /// Also write out split-kmers, and middle base matrix
+        #[arg(long, default_value_t = false)]
+        full_info: bool,
+    },
 
 }
-
-/*
---seqs <list.fa>  A fasta file with sequences to remove.
---min <freq>      Minimum frequency present to output to alignment [default: 0.9].
---const-sites     Write invariant sites.
---full-info       Write out split-kmers, and middle base matrix.
-
---cpus <cpus>     Use parallel processing, with this many cores.
-*/
 
 pub fn cli_args() -> Args {
     let args = Args::parse();
