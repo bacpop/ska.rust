d7c89acf155c572bd31d0f3a6745cd84571e18dd
John Lees <lees.john6@gmail.com>
2023-02-01 20:06:18 +0000

    Fix some more problems, but start another approach
    I think better to start with full length seq and write into it

--- src/ska_ref/aln_writer.rs
+++ src/ska_ref/aln_writer.rs
@@ -1,7 +1,6 @@
 
 use std::{error::Error, fmt};
 
-#[derive(Debug)]
 pub struct AlnWriter<'a> {
     next_pos: usize,
     curr_chrom: usize,
@@ -19,8 +18,7 @@ pub struct AlnWriter<'a> {
 impl<'a> AlnWriter<'a> {
     pub fn new(ref_seq: &'a Vec<Vec<u8>>, total_size: usize, k: usize) -> Self {
         let (curr_chrom, last_mapped, last_written) = (0, 0, 0);
-        let mut seq_out = Vec::new();
-        seq_out.reserve(total_size);
+        let seq_out = vec![b'-'; total_size];
         let half_split_len = (k - 1) / 2;
         let next_pos = half_split_len;
         Self {next_pos, curr_chrom, last_mapped, last_written, ref_seq, total_size, seq_out, half_split_len}
@@ -32,36 +30,43 @@ impl<'a> AlnWriter<'a> {
     // 1: Last written -> last map-pos + half-k
     // 2: Last mapped + half-k
     // 3: Mapped position
-    fn fill_to(&mut self, end: usize) {
+    fn fill_to(&mut self, end_position: usize, contig_end: bool) {
         // bases at the end of last valid match not yet written
-        let last_written_base = self.last_mapped + self.half_split_len;
-        let last_match_overhang = (self.last_mapped + self.half_split_len).saturating_sub(last_written_base);
-        if last_match_overhang > 0 {
-            self.seq_out.extend_from_slice(
-                &self.ref_seq[self.curr_chrom][last_written_base..(last_written_base + last_match_overhang)]
-            );
+        if self.last_written > 0 {
+            let last_match_overhang = (self.last_mapped + self.half_split_len).saturating_sub(self.last_written);
+            if last_match_overhang > 0 {
+                self.seq_out.extend_from_slice(
+                    &self.ref_seq[self.curr_chrom][(self.last_written + 1)..(self.last_written + last_match_overhang)]
+                );
+            }
         }
-
+        println!("{}", String::from_utf8(self.seq_out.clone()).unwrap());
         // 2-start of split k-mer: missing bases
-        let missing_bases = end.saturating_sub(self.last_mapped).saturating_sub(2 * self.half_split_len);
+        let mut missing_bases = end_position.saturating_sub(self.last_mapped + self.half_split_len);
+        // This accounts for the fact that at the start of the sequence no bases have
+        // been written yet
+        if !contig_end && self.last_written > 0 {
+            missing_bases = missing_bases.saturating_sub(self.half_split_len);
+        }
         if missing_bases > 0 {
             self.seq_out.extend(vec![b'-'; missing_bases]);
         }
-
+        println!("{}", String::from_utf8(self.seq_out.clone()).unwrap());
     }
 
     // Fill up to the end of the contig and update indices
     fn fill_contig(&mut self) {
-        self.fill_to(self.ref_seq[self.curr_chrom].len());
+        self.fill_to(self.ref_seq[self.curr_chrom].len(), true);
         self.curr_chrom += 1;
         self.next_pos = self.half_split_len;
     }
 
     pub fn write_split_kmer(&mut self, mapped_pos: usize, mapped_chrom: usize, base: u8) {
+        println!("{} {}", mapped_pos, base);
         if mapped_chrom > self.curr_chrom {
             self.fill_contig();
         }
-        if base != b'-' {
+        if base == b'-' {
             return;
         }
         if mapped_pos < self.next_pos {
@@ -70,7 +75,9 @@ impl<'a> AlnWriter<'a> {
         }
 
         // Write bases between last match and this one
-        self.fill_to(mapped_pos);
+        if mapped_pos > self.next_pos {
+            self.fill_to(mapped_pos, false);
+        }
 
         // First half of split k-mer
         self.seq_out.extend_from_slice(
@@ -82,9 +89,10 @@ impl<'a> AlnWriter<'a> {
         self.seq_out.extend_from_slice(
             &self.ref_seq[self.curr_chrom][(mapped_pos + 1)..=(mapped_pos + self.half_split_len)],
         );
+        println!("{}", String::from_utf8(self.seq_out.clone()).unwrap());
 
         // update indices
-        self.next_pos = mapped_pos + 2 * self.half_split_len;
+        self.next_pos = mapped_pos + 2 * self.half_split_len + 1;
         self.last_mapped = mapped_pos;
         self.last_written = mapped_pos + self.half_split_len;
 
@@ -92,8 +100,10 @@ impl<'a> AlnWriter<'a> {
     }
 
     pub fn get_seq(&'a mut self) -> Result<&'a[u8], Box<dyn self::Error + 'a>> {
+        println!("{}", String::from_utf8(self.seq_out.clone()).unwrap());
         self.fill_contig();
-        if self.ref_seq.len() != self.total_size {
+        println!("{}", String::from_utf8(self.seq_out.clone()).unwrap());
+        if self.seq_out.len() != self.total_size {
             Result::Err(Box::new(self))
         } else {
             Result::Ok(self.seq_out.as_slice())
@@ -105,8 +115,14 @@ impl Error for &mut AlnWriter<'_> {}
 
 impl fmt::Display for AlnWriter<'_> {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", String::from_utf8(self.seq_out.clone()).unwrap())
+    }
+}
+
+impl fmt::Debug for AlnWriter<'_> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "Output length {} not identical to chromosome length {}",
-        self.ref_seq.len(),
+        self.seq_out.len(),
         self.total_size)
     }
 }
