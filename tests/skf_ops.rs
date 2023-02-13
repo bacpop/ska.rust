f5041415d3412eca01d2e00778f467d8805beb6f
John Lees <lees.john6@gmail.com>
2023-02-13 18:48:19 +0000

    Add merge test for u128

--- tests/skf_ops.rs
+++ tests/skf_ops.rs
@@ -80,6 +80,80 @@ fn merge_delete() {
 }
 
 #[test]
+fn merge_delete_u128() {
+    let sandbox = TestSetup::setup();
+
+    Command::new(cargo_bin("ska"))
+        .current_dir(sandbox.get_wd())
+        .arg("build")
+        .arg(sandbox.file_string("test_1.fa", TestDir::Input))
+        .arg("-o")
+        .arg("test_1")
+        .arg("-k")
+        .arg("41")
+        .assert()
+        .success();
+
+    Command::new(cargo_bin("ska"))
+        .current_dir(sandbox.get_wd())
+        .arg("build")
+        .arg(sandbox.file_string("test_2.fa", TestDir::Input))
+        .arg("-o")
+        .arg("test_2")
+        .arg("-k")
+        .arg("41")
+        .assert()
+        .success();
+
+    Command::new(cargo_bin("ska"))
+        .current_dir(sandbox.get_wd())
+        .arg("merge")
+        .arg("test_1.skf")
+        .arg("test_2.skf")
+        .arg("-o")
+        .arg("merge")
+        .arg("-v")
+        .assert()
+        .success();
+
+    assert_eq!(true, sandbox.file_exists("merge.skf"));
+
+    // Try removing non-existent sample
+    Command::new(cargo_bin("ska"))
+        .current_dir(sandbox.get_wd())
+        .arg("delete")
+        .arg("test_3")
+        .arg("merge.skf")
+        .arg("-v")
+        .assert()
+        .failure();
+
+    // delete a sample then check nk same as original
+    let test1_nk = Command::new(cargo_bin("ska"))
+        .current_dir(sandbox.get_wd())
+        .arg("nk")
+        .arg("test_1.skf")
+        .output()
+        .unwrap();
+
+    Command::new(cargo_bin("ska"))
+        .current_dir(sandbox.get_wd())
+        .arg("delete")
+        .arg("merge.skf")
+        .arg("test_2")
+        .arg("-v")
+        .assert()
+        .success();
+
+    Command::new(cargo_bin("ska"))
+        .current_dir(sandbox.get_wd())
+        .arg("nk")
+        .arg("merge.skf")
+        .assert()
+        .stdout_eq(test1_nk.stdout);
+}
+
+#[test]
 fn weed() {
     let sandbox = TestSetup::setup();
 
